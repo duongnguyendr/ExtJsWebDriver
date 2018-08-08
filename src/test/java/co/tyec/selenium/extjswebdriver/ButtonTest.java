01775d909f1f54c9fe933125c078eb758d8e989c
duong.nguyen <duong.nguyen@titancorpvn.com>
2018-08-08 11:28:12 +0700

    Commit To Git

--- src/test/java/co/tyec/selenium/extjswebdriver/ButtonTest.java
+++ src/test/java/co/tyec/selenium/extjswebdriver/ButtonTest.java
@@ -3,11 +3,17 @@ package co.tyec.selenium.extjswebdriver;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+//import Project.FrameWork.common.Generic;
 
 /**
  * @author Asaf Levy
@@ -24,19 +30,44 @@ public class ButtonTest
 	
 	@Before
 	public void setUp() throws Exception {
-		driver = Mockito.mock(WebDriver.class, Mockito.withSettings().extraInterfaces(JavascriptExecutor.class));
-		js = (JavascriptExecutor) driver;
-		// Global Mocks
-		Mockito.when(((JavascriptExecutor) driver).executeScript(Mockito.contains("Ext.Ajax.isLoading()"))).thenReturn("false");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
+//        ChromeOptions options = setOptionsForChrome();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://movieworld.enovision.net/");
+//        js = (JavascriptExecutor) driver;
         byExtJsComponentQuery = new ByExtJsComponentQuery(driver);
+        Thread.sleep(10000);
+//        logger.info("***** Opened browser *****");
+		
+//		driver = Mockito.mock(WebDriver.class, Mockito.withSettings().extraInterfaces(JavascriptExecutor.class));
+//		js = (JavascriptExecutor) driver;
+//		// Global Mocks
+//		Mockito.when(((JavascriptExecutor) driver).executeScript(Mockito.contains("Ext.Ajax.isLoading()"))).thenReturn("false");
+//        byExtJsComponentQuery = new ByExtJsComponentQuery(driver);
 	}
 	
 	@Test
 	public void basicTest() {
-        final Button button = Mockito.spy(new Button(driver, byExtJsComponentQuery.byExtJsComponentQuery("[@text='pressed']")));
-		Mockito.doReturn("_id").when(button).getComponentId();
+//        final Button button = Mockito.spy(new Button(driver, byExtJsComponentQuery.byExtJsComponentQuery("[@text='pressed']")));
+//		Mockito.doReturn("_id").when(button).getComponentId();
+//		String id = button.getComponentId();
+//		assertEquals("id is incorrect", "_id", button.getComponentId());
+		System.out.println("Button: ");
+		System.out.println("Driver: " + driver);
+		System.out.println("Js: " + js);
+		ByExtJsComponentQuery qr = byExtJsComponentQuery.byExtJsComponentQuery("button[text='Who worked with who']");
+		System.out.println("qrrrrrrr : " + qr);
+		Button button = new Button(driver, byExtJsComponentQuery.byExtJsComponentQuery("button[text='Who worked with who']"));
+		System.out.println("Button: " + button);
 		String id = button.getComponentId();
-		assertEquals("id is incorrect", "_id", button.getComponentId());
+		button.click();
+		try {
+			Thread.sleep(10000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 	
 }
